0c88a71f59f64937f7bcf97b46300e85bf25496a
Michael <madetunji1376@gmail.com>
2023-09-19 16:57:47 +0100

    task completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import image from "./assets/images/image-qr-code.png";
+import "./App.css";
 
 const App = () => {
-
   return (
-    <>
-      <h1>React App</h1>
-    </>
-  )
-}
+    <section>
+      <div className="container max-w-sm overflow-hidden shadow-lg mx-auto my-4 p-5 
+      ">
+          <img src={image} alt="QR Code" className="rounded" />
+          <div className="text-center">
+            <h2 className="header text-2xl mt-3
+            ">
+              Improve your front-end skills by building projects
+            </h2>
+            <p className="paragraph text-sm text-xl mt-5 mb-5
+            ">
+              Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+            </p>
+          </div>
+      </div>
+    </section>
+  );
+};
 
-export default App
+export default App;
